0a48f419e0b13333ee19e30f5b886c9d36fd825a
한유경 <ygo65312@naver.com>
2023-04-24 16:18:10 +0900

    ✅ update tailwindcss color

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class',
 	content: ['./home.tsx', './src/**/*.{ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				green: '#00C981',
+				yellow: '#FFC702',
+				red: '#FF004D',
+				skyBlue: '#A8DAF7',
+				paleyellow: '#FFF0BB',
+				blue: '#00A3FF',
+				pink: '#FF7373',
+				black: '#000000',
+				white: '#FFFFFF',
+				gray: '#EEEEEE',
+				'dark/background': '#000000',
+				'dark/component': '#202020',
+				'dark/stroke': '#444444',
+				'dark/boldStroke': '#AAAAAA',
+				'dark/text': '#FFFFFF',
+				'light/background': '#F8F8F8',
+				'light/component': '#FFFFFF',
+				'light/stroke': '#EEEEEE',
+				'light/boldStroke': '#AAAAAA',
+				'light/text': '#000000',
+			},
+		},
 	},
 	plugins: [],
 };
