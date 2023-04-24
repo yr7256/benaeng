f569f41a4f46bce56e11d0e88233060fe04412e7
한유경 <ygo65312@naver.com>
2023-04-24 16:21:26 +0900

    🎨 add page/index.ts

---
+++ fe/src/pages/index.ts
@@ -0,0 +1,7 @@
+export { default as Analysis } from './Analysis';
+export { default as BarcodeReader } from './BarcodeReader';
+export { default as FoodDetail } from './FoodDetail';
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Setting } from './Setting';
+export { default as Notice } from './Notice';
