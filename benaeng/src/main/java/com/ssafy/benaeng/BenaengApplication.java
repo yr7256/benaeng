8763bb074e3bac2292dd634eece0e7f3922f9bde
seunghee <kanada11416@gmail.com>
2023-04-24 17:47:06 +0900

    🎉 init project

---
+++ benaeng/src/main/java/com/ssafy/benaeng/BenaengApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.benaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenaengApplication.class, args);
+	}
+
+}
