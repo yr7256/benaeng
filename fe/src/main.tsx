5c4fad61955a274c65a6f175a8e14de0d109e492
서예지 <syg9272@naver.com>
2023-04-25 10:30:24 +0900

    🩹 fix simple error

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import { Provider } from 'react-redux';
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
 import store from './store/store';
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export const persistor = persistStore(store);
 
