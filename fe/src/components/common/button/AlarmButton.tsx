e1f78a3a7fa9172e0d609d8b0f517fac6b2d9353
서예지 <syg9272@naver.com>
2023-04-26 12:16:07 +0900

    🎨 add alarm button

---
+++ fe/src/components/common/button/AlarmButton.tsx
@@ -0,0 +1,25 @@
+interface Props {
+	isAlarm: boolean;
+}
+
+function AlarmButton({ isAlarm }: Props) {
+	/** 알람 버튼 클릭 이벤트 */
+	const handleAlarm = () => {
+		window.location.href = '/notice';
+	};
+	return (
+		<button type="button" className="relative w-12 h-12 cursor-pointer" onClick={handleAlarm}>
+			{isAlarm && (
+				<span className="absolute flex w-3 h-3 top-0.5 right-0.5">
+					<span className="absolute inline-flex w-full h-full rounded-full opacity-75 animate-ping bg-red" />
+					<span className="relative inline-flex w-3 h-3 rounded-full bg-red" />
+				</span>
+			)}
+			{/* {isAlarm && <div className="absolute w-2 h-2 rounded-lg right-1 top-1 bg-red" />} */}
+			<div className="flex items-center w-12 h-12 border-2 rounded-8 bg-light/component dark:bg-dark/component stroke">
+				<img className="block w-6 h-6 m-auto" src="../src/assets/common/alarm.svg" alt="alarm" />
+			</div>
+		</button>
+	);
+}
+export default AlarmButton;
