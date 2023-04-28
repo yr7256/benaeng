d49d891922e537b929fa721275846c5412bf2cad
서예지 <syg9272@naver.com>
2023-04-28 11:53:56 +0900

    🎨 add kakao login

--- fe/src/components/common/button/LoginButton.tsx
+++ fe/src/components/common/button/LoginButton.tsx
@@ -1,28 +1,17 @@
-// import { useEffect } from 'react';
+import { REDIRECT_URI, REST_API_KEY } from '../../../constants/api';
 
 function LoginButton() {
-	// useEffect(() => {
-	// 	// 카카오 SDK 초기화
-	// 	if (!window.Kakao.isInitialized()) {
-	// 		// 초기화 되어있지 않을 경우(중복 초기화 에러 방지)
-	// 		window.Kakao.init(process.env.REACT_APP_KAKAO_API_KEY); // env 환경변수 사용
-	// 		// console.log(window.Kakao.isInitialized()); // 초기화 여부 확인(true 나와야 함)
-	// 	}
-	// }, []);
-
 	/** 카카오 로그인 */
-	// const handleLogin = () => {
-	// 	window.Kakao.Auth.authorize({
-	// 		redirectUri: process.env.REACT_APP_KAKAO_REDIRECTURI,
-	// 	});
-	// 	window.location.href = process.env.REACT_APP_LOGIN_REDIRECTURI ? process.env.REACT_APP_LOGIN_REDIRECTURI : '/';
-	// };
+	const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+	const handleLogin = () => {
+		window.location.href = KAKAO_AUTH_URI;
+	};
 
 	return (
 		<button
 			type="button"
 			className="flex flex-row items-center justify-center h-20 w-80 bg-kakao hover:bg-[#AF7B2D] rounded-2xl"
-			// onClick={handleLogin}
+			onClick={handleLogin}
 		>
 			<img className="w-12 h-12" src="/assets/common/kakao.svg" alt="kakao" />
 			<p className="ml-12 text-xl">카카오로 시작하기</p>
