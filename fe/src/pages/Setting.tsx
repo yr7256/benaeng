20afc757c14a9e76d83979a1c9d87b38ed7ceaf8
서예지 <syg9272@naver.com>
2023-04-28 15:08:58 +0900

    🎨 update setting page

--- fe/src/pages/Setting.tsx
+++ fe/src/pages/Setting.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import Topbar from '../components/common/topbar/Topbar';
+import { useAppSelector } from '../hooks/useStore';
+import { selectUser } from '../store/modules/user';
+import Toggle from '../components/common/toggle/Toggle';
 
 // 설정 화면
 
 function Setting() {
-	return <div>Setting</div>;
+	const userInfo = useAppSelector(selectUser);
+	return (
+		<div>
+			<Topbar />
+			<div className="component">
+				<div>다크모드</div>
+				<Toggle isCheck={false} onState={userInfo.isDark} type="isDark" />
+			</div>
+			<div className="component">
+				<div>알림 받기</div>
+				<Toggle isCheck={false} onState={userInfo.isAlarm} type="isAlarm" />
+			</div>
+			<div className="component">
+				<div>
+					<div>소비기한 임박 식품 알림</div>
+					<Toggle isCheck={!userInfo.isAlarm} onState={userInfo.isAlarm ? userInfo.isCycle : false} type="isCycle" />
+				</div>
+				<div>
+					<div>식품 구매주기 알림</div>
+					<Toggle
+						isCheck={!userInfo.isAlarm}
+						onState={userInfo.isAlarm ? userInfo.isPurchase : false}
+						type="isPurchase"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Setting;
