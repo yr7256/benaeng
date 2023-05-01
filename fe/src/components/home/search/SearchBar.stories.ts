a0f3dd8c5e5954156cd1b0755de53439344a8ca8
한유경 <ygo65312@naver.com>
2023-05-01 12:39:23 +0900

    🎨 add search bar component

---
+++ fe/src/components/home/search/SearchBar.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react';
+import SearchBar from './SearchBar';
+
+const meta = {
+	title: 'home/SearchBar',
+	component: SearchBar,
+	tags: ['autodocs'],
+	argTypes: {
+		value: {
+			defaultValue: '',
+			type: 'string',
+		},
+	},
+} satisfies Meta<typeof SearchBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		value: '',
+		setValue(value) {
+			alert(value);
+		},
+	},
+};
