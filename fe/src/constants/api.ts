db73d7198d649a0c53d71b9b700064ff407af312
서예지 <syg9272@naver.com>
2023-05-01 14:38:51 +0900

    🎨 add api uri

--- fe/src/constants/api.ts
+++ fe/src/constants/api.ts
@@ -1,2 +1,15 @@
-export const REST_API_KEY = import.meta.env.REACT_APP_REST_API_KEY;
+export const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
 export const REDIRECT_URI = 'http://localhost:3000/login';
+
+// 기본 base 경로
+export const BASE_URL = 'https://k8b205.p.ssafy.io';
+
+// 토큰 받기
+export const SOCIAL = '/api/social';
+// 사용자 정보 조회
+export const USER = '/api/user';
+
+// stale time
+export const STALE_TIME = 1000 * 60 * 5;
+// cache time
+export const CACHE_TIME = 1000 * 60 * 20;
