6fcd63c7c0311124aa4db81e930c0a0cb41efeda
seunghee <kanada11416@gmail.com>
2023-05-02 17:45:18 +0900

    ✨ get user information

--- benaeng/src/main/java/com/ssafy/benaeng/domain/user/service/UserService.java
+++ benaeng/src/main/java/com/ssafy/benaeng/domain/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ssafy.benaeng.domain.user.service;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.ssafy.benaeng.domain.user.entity.User;
 import com.ssafy.benaeng.domain.user.repository.UserRepository;
@@ -15,6 +16,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -83,6 +85,53 @@ public class UserService {
             e.printStackTrace();
         }
         return accessToken;
+    }
+    public HashMap<String, Object> getUserInfo (String accessToken) {
+        log.info("--------------------getUserInfo of User Service--------------------");
+        //    요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+        HashMap<String, Object> userInfo = new HashMap<>();
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            //    요청에 필요한 Header에 포함될 내용
+            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+            int responseCode = conn.getResponseCode();
+            log.info("responseCode : " + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            log.info("response body : " + result);
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+            log.info("get id : " + element.getAsJsonObject().get("id"));
+            Long kakaoId = element.getAsJsonObject().get("id").getAsBigInteger().longValue();
+            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+
+            String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+            String imageUrl = properties.getAsJsonObject().get("profile_image").getAsString();
+            log.info("kakaoId : " + kakaoId);
+            log.info("nickname : " + nickname);
+            log.info("imageUrl : " + imageUrl);
+
+            userInfo.put("kakaoId", kakaoId);
+            userInfo.put("nickname", nickname);
+            userInfo.put("imageUrl", imageUrl);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return userInfo;
     }
 
 
