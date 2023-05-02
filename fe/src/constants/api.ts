f45803968f41f9ecd35585d6694175eadf3c128d
서예지 <syg9272@naver.com>
2023-05-02 12:24:34 +0900

    🎨 update uri

--- fe/src/constants/api.ts
+++ fe/src/constants/api.ts
@@ -2,12 +2,12 @@ export const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
 export const REDIRECT_URI = 'http://localhost:3000/login';
 
 // 기본 base 경로
-export const BASE_URL = 'http://192.168.31.89:8080';
+export const BASE_URL = 'http://192.168.31.89:8080/api';
 
 // 토큰 받기
-export const SOCIAL = '/api/social';
+export const SOCIAL = '/social';
 // 사용자 정보 조회
-export const USER = '/api/user';
+export const USER = '/user';
 
 // stale time
 export const STALE_TIME = 1000 * 60 * 5;
