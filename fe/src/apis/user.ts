52809b9436a997e5c27f3ad61cfbf3ff3fe3576f
서예지 <syg9272@naver.com>
2023-05-02 12:24:19 +0900

    🎨 update query

--- fe/src/apis/user.ts
+++ fe/src/apis/user.ts
@@ -1,6 +1,5 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import { useQuery } from '@tanstack/react-query';
-import { BASE_URL, CACHE_TIME, SOCIAL, STALE_TIME } from '../constants/api';
+import axios from 'axios';
+import { BASE_URL, SOCIAL } from '../constants/api';
 
 export const useGetSocial = (code: string) => {
 	//   요청 url
@@ -8,13 +7,7 @@ export const useGetSocial = (code: string) => {
 	//   axios 요청
 	const queryFn = axios.get(queryKey).then(res => res.data);
 
-	const { isLoading, data, isError, error } = useQuery<AxiosResponse, AxiosError>([SOCIAL], () => queryFn, {
-		keepPreviousData: true,
-		staleTime: STALE_TIME,
-		cacheTime: CACHE_TIME,
-	});
-
-	return { isLoading, data, isError, error };
+	return queryFn;
 };
 
 export default useGetSocial;
--- fe/src/constants/api.ts
+++ fe/src/constants/api.ts
@@ -2,7 +2,7 @@ export const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
 export const REDIRECT_URI = 'http://localhost:3000/login';
 
 // 기본 base 경로
-export const BASE_URL = 'https://k8b205.p.ssafy.io';
+export const BASE_URL = 'http://192.168.31.89:8080';
 
 // 토큰 받기
 export const SOCIAL = '/api/social';
