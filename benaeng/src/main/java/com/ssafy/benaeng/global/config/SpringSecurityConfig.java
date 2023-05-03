6e8e89e743304773cbe9d891a8ba40d2c54847d4
seunghee <kanada11416@gmail.com>
2023-05-03 13:54:13 +0900

    🔧 add PasswordEncoder

--- benaeng/src/main/java/com/ssafy/benaeng/global/config/SpringSecurityConfig.java
+++ benaeng/src/main/java/com/ssafy/benaeng/global/config/SpringSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -44,4 +46,8 @@ public class SpringSecurityConfig {
         return source;
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
