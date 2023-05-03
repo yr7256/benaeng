e0d9a8377f0d6f72ad91c3b5418c5375bb197cf1
한유경 <ygo65312@naver.com>
2023-05-03 17:42:31 +0900

    🎨 update style

--- fe/src/pages/BarcodeReader.tsx
+++ fe/src/pages/BarcodeReader.tsx
@@ -26,7 +26,7 @@ function BarcodeReader() {
 			video.srcObject = stream;
 
 			video.addEventListener('play', () => {
-				const maxSize = 320;
+				const maxSize = 480;
 				const videoSize = Math.min(video.videoHeight, video.videoWidth);
 
 				// 캔버스 설정
@@ -70,39 +70,34 @@ function BarcodeReader() {
 	};
 
 	return (
-		<div className="center w-screen h-screen box-border bg-black overflow-hidden">
-			<div className="flex flex-col items-center max-w-xl h-full box-border overflow-hidden">
-				{/* 헤더 */}
-				<div className="min-h-[12%] h-[4rem] text-white text-xs flex flex-col justify-end items-center p-5">
-					화면 중앙에 바코드를 스캔해주세요
-				</div>
+		<div className="center flex-col w-screen h-screen box-border bg-black overflow-hidden">
+			{/* 헤더 */}
+			<div className="min-h-[12%] h-[4rem] text-white text-xs flex flex-col justify-end items-center p-5">
+				화면 중앙에 바코드를 스캔해주세요
+			</div>
 
-				{/* 비디오 공간 */}
-				<div className="min-w-full flex-1 relative center">
-					<video className="h-full object-cover" autoPlay muted playsInline />
-					<div className="absolute top-0 w-full h-full bg-black/20 backdrop-blur-sm" />
-					<canvas className="camera-canvas absolute z-1" />
-				</div>
+			{/* 비디오 공간 */}
+			<div className="min-w-full flex-1 relative center">
+				<video className="h-full object-cover" autoPlay muted playsInline />
+				<div className="absolute top-0 w-full h-full bg-black/20 backdrop-blur-sm" />
+				<canvas className="camera-canvas absolute z-1" />
+			</div>
 
-				{/* 제어버튼 공간 */}
-				<div className="max-w-4xl w-full min-h-[15%] m-4 pb-2 center relative">
-					{/* 촬영 버튼 */}
-					<button
-						type="button"
-						onClick={onCapture}
-						className="w-20 box-border aspect-square rounded-full border-solid border-[0.5rem] border-white bg-green center"
-					>
-						<TbCamera className="text-4xl text-white" />
-					</button>
+			{/* 제어버튼 공간 */}
+			<div className="max-w-4xl w-full min-h-[15%] m-4 pb-2 center relative">
+				{/* 촬영 버튼 */}
+				<button
+					type="button"
+					onClick={onCapture}
+					className="w-20 box-border aspect-square rounded-full border-solid border-[0.5rem] border-white bg-green center"
+				>
+					<TbCamera className="text-4xl text-white" />
+				</button>
 
-					{/* 직접 입력 버튼 */}
-					<button
-						type="button"
-						className="min-w-[25%] w-20 py-6 m-6 absolute right-0 text-green font-extrabold text-sm"
-					>
-						직접 입력
-					</button>
-				</div>
+				{/* 직접 입력 버튼 */}
+				<button type="button" className="min-w-[25%] w-20 py-6 m-4 absolute right-0 text-green font-extrabold text-sm">
+					직접 입력
+				</button>
 			</div>
 
 			<Modal
