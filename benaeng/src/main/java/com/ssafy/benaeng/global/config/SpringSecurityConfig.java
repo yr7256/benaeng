a93cf9e87134fd662f4e7b9446068aa1f380ac4c
seunghee <kanada11416@gmail.com>
2023-05-03 17:41:04 +0900

    ✨ Authenticate the JWT in the request

--- benaeng/src/main/java/com/ssafy/benaeng/global/config/SpringSecurityConfig.java
+++ benaeng/src/main/java/com/ssafy/benaeng/global/config/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ssafy.benaeng.global.config;
 
+import com.ssafy.benaeng.domain.user.jwt.JwtTokenProvider;
+import com.ssafy.benaeng.global.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,7 +22,8 @@ import java.util.Arrays;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
-//    private final UserServiceOld userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         // cors 구성 설정
@@ -32,7 +36,8 @@ public class SpringSecurityConfig {
                 .antMatchers("/api/social/**").permitAll()      // 로그인 안했을 경우 /api/social/** 요청만 가능
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("http://localhost:3000/login").permitAll();      // 로그인 안했을 경우 해당 URL로 보내기
+                .formLogin().loginPage("http://localhost:3000/login").permitAll()      // 로그인 안했을 경우 해당 URL로 보내기
+                .and().addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ benaeng/src/main/java/com/ssafy/benaeng/global/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package com.ssafy.benaeng.global.filter;
+
+import com.ssafy.benaeng.domain.user.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * HttpServletRequest에서 토큰을 추출하고 토큰에 대한 유효성을 검사하는 필터
+ */
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+       if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
