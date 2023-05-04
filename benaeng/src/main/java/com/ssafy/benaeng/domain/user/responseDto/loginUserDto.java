71cc72ce282d4656fdcc7139f3d68d756cf5e463
seunghee <kanada11416@gmail.com>
2023-05-04 12:51:39 +0900

    ✨ add @Getter annotation(loginUserDto)

--- benaeng/src/main/java/com/ssafy/benaeng/domain/user/responseDto/loginUserDto.java
+++ benaeng/src/main/java/com/ssafy/benaeng/domain/user/responseDto/loginUserDto.java
@@ -2,10 +2,12 @@ package com.ssafy.benaeng.domain.user.responseDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Getter
 @Builder
 public class loginUserDto {
     private Boolean isDark;
