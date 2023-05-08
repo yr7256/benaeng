46a8709c61c52198c1dae42f0aebd591ee2d7d06
이진섭 <jjjp9966@gmail.com>
2023-05-08 12:55:05 +0900

    :white_check_mark: add logging to verify input value

--- benaeng/src/main/java/com/ssafy/benaeng/domain/fcm/controller/FCMController.java
+++ benaeng/src/main/java/com/ssafy/benaeng/domain/fcm/controller/FCMController.java
@@ -2,6 +2,7 @@ package com.ssafy.benaeng.domain.fcm.controller;
 
 import com.ssafy.benaeng.domain.fcm.service.FCMService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/fcm")
@@ -17,5 +19,6 @@ public class FCMController {
 
     @PostMapping
     public void getToken(@RequestBody Map<String, String> request) {
+        log.info("{}", request.get("deviceToken"));
     }
 }
