2b68a6585ee31e664c734eb73adba93f41e00a50
서예지 <syg9272@naver.com>
2023-05-08 17:53:06 +0900

    🎨 update food-detail data

--- fe/src/components/foods/analysis/FoodDetailAnalysis.tsx
+++ fe/src/components/foods/analysis/FoodDetailAnalysis.tsx
@@ -1,3 +1,5 @@
+import { useAppSelector } from '../../../hooks/useStore';
+import { selectUser } from '../../../store/modules/user';
 import { FoodDetailData } from '../../../types';
 import CircleGraph from '../../common/circleGraph/CircleGraph';
 
@@ -6,6 +8,9 @@ interface Props {
 }
 
 function FoodDetailAnalysis({ foodData }: Props) {
+	const user = useAppSelector(selectUser);
+	const box = `/assets/${user.isDark ? 'dark' : 'light'}/empty-box.svg`;
+	const refrigerator = `/assets/${user.isDark ? 'dark' : 'light'}/empty-refrigerator.svg`;
 	return (
 		<div className="w-full h-auto px-4 mx-auto my-6">
 			<div className="w-full mb-4 font-bold text-left ">
@@ -13,20 +18,20 @@ function FoodDetailAnalysis({ foodData }: Props) {
 				<span className="text-text"> 소비패턴 요약</span>
 			</div>
 			<div className="flex items-center justify-between mb-4">
-				<div className="flex-col w-full h-[109px] text-xs relative">
+				<div className="flex-col w-full h-[109px] text-sm relative">
 					<img className="block w-20 h-20 mx-auto mb-2" src="/assets/common/purchase.svg" alt="purchase" />
 					<div className="absolute z-10 text-2xl font-black -translate-x-1/2 left-1/2 top-6 text-yellow text-inherit whitespace-nowrap text-stroke">
 						{foodData.purchase}일
 					</div>
 					<div>평균 구매주기</div>
 				</div>
-				<div className="flex-col w-full h-[109px] text-xs">
+				<div className="flex-col w-full h-[109px] text-sm">
 					<div className="flex justify-center mb-2">
 						<CircleGraph reverse size="sm" percent={foodData.percent / 100} />
 					</div>
 					<div>폐기율</div>
 				</div>
-				<div className="flex-col w-full h-[109px] text-xs relative">
+				<div className="flex-col w-full h-[109px] text-sm relative">
 					<img className="block w-20 h-20 mx-auto mb-2" src="/assets/common/cycle.svg" alt="cycle" />
 					<div className="absolute z-10 text-2xl font-black -translate-x-1/2 left-1/2 top-6 text-blue whitespace-nowrap text-stroke">
 						{foodData.cycle}일
@@ -35,35 +40,46 @@ function FoodDetailAnalysis({ foodData }: Props) {
 				</div>
 			</div>
 			<div className="w-full px-6 pt-4 pb-2 mb-12 text-xs text-left border rounded-lg stroke">
-				<div className="mb-2">
-					<span className="font-bold text-green">{foodData.subCategory}</span>을(를) 많이 소비하고 있어요
-				</div>
-				<div className="mb-2">
-					소비기한 내 <span className="font-bold text-green">{foodData.subCategory}</span>을(를) 소비하지 못하고 있어요
-				</div>
-				<div className="mb-2 font-bold">
-					더 작은 크기의 <span className="font-extrabold text-green">{foodData.subCategory}</span>을(를) 구매해보세요!
-				</div>
+				{foodData.msg &&
+					foodData.msg.map((item, index) => {
+						if (index !== 2) {
+							return <div className="mb-2">{item}</div>;
+						}
+						return <div className="mb-2 font-bold">{item}</div>;
+					})}
+				{!foodData.msg && (
+					<div className="mb-2 text-center">
+						<img className="block w-24 m-auto" src={box} alt="msg" />
+						<div className="mt-4 text-sm">분석 결과가 존재하지 않습니다.</div>
+					</div>
+				)}
 			</div>
 			<div className="w-full">
 				<div className="pl-2 mb-4 text-sm font-bold text-left">자주 구매하는 상품</div>
-				<div className="w-full h-[130px] flex flex-col justify-between">
-					{foodData.preferProduct.map(item => (
-						<div
-							key={item}
-							className="flex items-center rounded-[16px] px-1 justify-between w-full h-[34px] border stroke"
-						>
-							<div className="ml-3 text-xs">{item}</div>
+				<div className="flex flex-col justify-between w-full h-auto">
+					{foodData.preferProducts &&
+						foodData.preferProducts.map(item => (
 							<div
-								className="flex items-center w-6 h-6 cursor-pointer rounded-xl bg-paleyellow"
-								onClick={() => {
-									window.location.href = `https://www.coupang.com/np/search?component=&q=${item}&channel=user`;
-								}}
+								key={item}
+								className="flex items-center rounded-[16px] px-1 justify-between w-full h-[34px] border stroke"
 							>
-								<img className="block w-4 h-4 m-auto" src="/assets/common/cart.svg" alt="cart" />
+								<div className="ml-3 text-sm">{item}</div>
+								<div
+									className="flex items-center w-6 h-6 cursor-pointer rounded-xl bg-paleyellow"
+									onClick={() => {
+										window.location.href = `https://www.coupang.com/np/search?component=&q=${item}&channel=user`;
+									}}
+								>
+									<img className="block w-4 h-4 m-auto" src="/assets/common/cart.svg" alt="cart" />
+								</div>
 							</div>
+						))}
+					{!foodData.preferProducts && (
+						<div className="w-full px-6 pt-4 pb-4 mb-2 text-xs text-left border rounded-lg stroke">
+							<img className="block w-24 m-auto" src={refrigerator} alt="msg" />
+							<div className="mt-4 text-sm">아직 구매한 상품이 존재하지 않습니다.</div>
 						</div>
-					))}
+					)}
 				</div>
 			</div>
 		</div>
