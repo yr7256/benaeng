c5cdd498124487160c3d5396edd716eefa8e90d8
이진섭 <jjjp9966@gmail.com>
2023-05-08 14:03:21 +0900

    :sparkles: add exclude attribute value

--- benaeng/src/main/java/com/ssafy/benaeng/BenaengApplication.java
+++ benaeng/src/main/java/com/ssafy/benaeng/BenaengApplication.java
@@ -6,7 +6,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @EnableScheduling
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(
+		exclude = {
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		}
+)
 public class BenaengApplication {
 
 	public static void main(String[] args) {
