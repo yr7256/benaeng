2fb5c7629bc9cb409401b72d419997dd7ccb8f88
이진섭 <jjjp9966@gmail.com>
2023-05-09 17:07:13 +0900

    :wrench: update SpringSecurityConfig

--- benaeng/src/main/java/com/ssafy/benaeng/global/config/SpringSecurityConfig.java
+++ benaeng/src/main/java/com/ssafy/benaeng/global/config/SpringSecurityConfig.java
@@ -23,36 +23,39 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         // cors 구성 설정
-        http.cors().configurationSource(corsConfigurationSource())
-                .and()
+//        http.cors().configurationSource(corsConfigurationSource())
+//                .and()
+        http
                 .csrf().disable()   // CSRF 보호 기능 해제
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)// 세션을 사용하지 않는 것. 서버에 상태 저장 X
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/social/**", "/api/fcm/**").permitAll()      // 로그인 안했을 경우 /api/social/** 요청만 가능
+                .antMatchers("/api/**").permitAll()      // 로그인 안했을 경우 /api/social/** 요청만 가능
                 .anyRequest().authenticated()
 //                .and()
 //                .formLogin().loginPage("http://localhost:3000/login").permitAll()      // 로그인 안했을 경우 해당 URL로 보내기
-                .and().addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+                .and().addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.addExposedHeader("*");
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.addAllowedOrigin("http://localhost:3000");
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "DELETE"));
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.addExposedHeader("*");
+//        configuration.setAllowCredentials(true);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
