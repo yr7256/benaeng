c7918cd8983968027bbf153849d7ed49a37fe17a
yr7256 <yr7256@gmail.com>
2023-05-09 11:15:14 +0900

    🎨 edit post url

--- fe/src/constants/api.ts
+++ fe/src/constants/api.ts
@@ -3,6 +3,7 @@ export const REDIRECT_URI = 'https://k8b205.p.ssafy.io/login';
 
 // 기본 base 경로
 export const BASE_URL = 'https://k8b205.p.ssafy.io';
+export const BASE_HTTP_URL = 'http://k8b205.p.ssafy.io';
 
 // stale time
 export const STALE_TIME = 1000 * 60 * 5;
