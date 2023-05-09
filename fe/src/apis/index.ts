4df4ae68b31d762c94e9a2a03ae92385f3dfe68a
서예지 <syg9272@naver.com>
2023-05-09 09:19:22 +0900

    🎨 update instance : add headers with token

--- fe/src/apis/index.ts
+++ fe/src/apis/index.ts
@@ -1,15 +1,20 @@
 import axios from 'axios';
 import { BASE_URL } from '../constants/api';
+import { getCookie } from '../utils/cookie';
 
 // 공통 인스턴스
-const instance = axios.create({
-	baseURL: `${BASE_URL}/api`,
-	timeout: 2000,
-	headers: {
-		'Access-Control-Allow-Origin': BASE_URL,
-		'Content-Type': 'application/json',
-	},
-	withCredentials: true,
-});
+function getInstance() {
+	const instance = axios.create({
+		baseURL: `${BASE_URL}/api`,
+		timeout: 2000,
+		headers: {
+			Authorization: `Bearer ${getCookie('accessToken')}`,
+			'Access-Control-Allow-Origin': BASE_URL,
+			'Content-Type': 'application/json',
+		},
+		withCredentials: true,
+	});
+	return instance;
+}
 
-export default instance;
+export default getInstance;
--- fe/src/apis/user.ts
+++ fe/src/apis/user.ts
@@ -1,27 +1,18 @@
-import instance from '.';
+import getInstance from '.';
 import { UserData } from '../types/UserTypes';
-import { getCookie } from '../utils/cookie';
 
 export const SOCIAL_API = '/social';
 export const USER_API = '/user';
 
 export function useGetSocial(code: string) {
-	return instance.get(`${SOCIAL_API}/${code}`);
+	return getInstance().get(`${SOCIAL_API}/${code}`);
 }
 
 export function usePutUser(user: UserData) {
-	return instance.put(
-		USER_API,
-		{
-			isAlarm: user.isAlarm,
-			isCycle: user.isCycle,
-			isDark: user.isDark,
-			isPurchase: user.isPurchase,
-		},
-		{
-			headers: {
-				Authorization: `Bearer ${getCookie('accessToken')}`,
-			},
-		},
-	);
+	return getInstance().put(USER_API, {
+		isAlarm: user.isAlarm,
+		isCycle: user.isCycle,
+		isDark: user.isDark,
+		isPurchase: user.isPurchase,
+	});
 }
