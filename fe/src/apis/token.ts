3408354947836b325fed29c4576f5c7208ada8f9
yr7256 <yr7256@gmail.com>
2023-05-09 15:28:48 +0900

    ✅ edit sendToken

--- fe/src/apis/token.ts
+++ fe/src/apis/token.ts
@@ -5,7 +5,5 @@ const FCM_API = '/fcm';
 
 /** [POST] FCM 토큰 전송 */
 export default function sendToken(deviceToken: string) {
-	return getInstance()
-		.post<Response<null>>(`${FCM_API}`, { deviceToken })
-		.then(response => response.data);
+	return getInstance().post<Response<null>>(`${FCM_API}`, { deviceToken });
 }
