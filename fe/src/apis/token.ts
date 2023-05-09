594560782400b9eff30901aa0793425c65a6e556
yr7256 <yr7256@gmail.com>
2023-05-09 16:12:53 +0900

    ✅ add post test

--- fe/src/apis/token.ts
+++ fe/src/apis/token.ts
@@ -4,10 +4,10 @@ import { Response } from '../types';
 const FCM_API = '/test';
 
 /** [POST] FCM 토큰 전송 */
-// export default function sendToken(deviceToken: string) {
-// 	return getInstance().post<Response<null>>(`${FCM_API}`, { deviceToken });
-// }
-
-export default function sendToken() {
-	return getInstance().get<Response<null>>(`${FCM_API}`);
+export default function sendToken(deviceToken: string) {
+	return getInstance().post<Response<null>>(`${FCM_API}`, { deviceToken });
 }
+
+// export default function sendToken() {
+// 	return getInstance().get<Response<null>>(`${FCM_API}`);
+// }
