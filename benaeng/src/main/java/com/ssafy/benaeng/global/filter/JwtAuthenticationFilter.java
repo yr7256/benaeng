2df2c08925d3dac35d58d25519749fdf6226a39d
이진섭 <jjjp9966@gmail.com>
2023-05-10 11:56:50 +0900

    :fire: delete @Component annotation

--- benaeng/src/main/java/com/ssafy/benaeng/global/filter/JwtAuthenticationFilter.java
+++ benaeng/src/main/java/com/ssafy/benaeng/global/filter/JwtAuthenticationFilter.java
@@ -4,7 +4,6 @@ import com.ssafy.benaeng.domain.user.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -17,7 +16,6 @@ import java.io.IOException;
  * HttpServletRequest에서 토큰을 추출하고 토큰에 대한 유효성을 검사하는 필터
  */
 @RequiredArgsConstructor
-@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -25,7 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
-       if (token != null && jwtTokenProvider.validateToken(token)) {
+        if (token != null && jwtTokenProvider.validateToken(token)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
