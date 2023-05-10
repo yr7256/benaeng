364045ce8cea47d225abe9e1f1492247bd48ebe3
한유경 <ygo65312@naver.com>
2023-05-10 15:36:52 +0900

    🎨 update getBarcodeData request

--- fe/src/hooks/useBarcode.ts
+++ fe/src/hooks/useBarcode.ts
@@ -1,45 +1,51 @@
+import { useQuery } from '@tanstack/react-query';
 import { BarcodeFormat, DecodeHintType, BrowserMultiFormatReader } from '@zxing/library';
-import { useEffect } from 'react';
-import { getBarcodeData, resetBarcodeData, useEmptyBarcodeData } from '../store/modules/barcode';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { resetBarcodeData, useEmptyBarcodeData, getBarcodeData, selectBarcode } from '../store/modules/barcode';
 import getStream from '../utils/camera';
-import { useAppDispatch } from './useStore';
+import { useAppDispatch, useAppSelector } from './useStore';
 
 function useBarcode() {
+	const barcode = useAppSelector(selectBarcode);
+	const navigate = useNavigate();
+
 	const dispatch = useAppDispatch();
+
+	const [code, setCode] = useState<string>('');
+	useQuery(['barcode', code], () => dispatch(getBarcodeData(code)), {
+		enabled: Boolean(code),
+	});
+
 	const hints = new Map();
 	const formats = [BarcodeFormat.EAN_13, BarcodeFormat.EAN_8, BarcodeFormat.CODE_128];
 	hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
 	const reader = new BrowserMultiFormatReader(hints);
+
+	useEffect(() => {
+		if (barcode.status === 'success') navigate('/');
+		if (barcode.status === 'fail') setCode('');
+	}, [barcode.status]);
 
 	// 초기 비디오 설정
 	useEffect(() => {
 		dispatch(resetBarcodeData);
 
 		const video = document.getElementsByTagName('video')[0];
-		const canvas = document.getElementsByTagName('canvas')[0];
-		const ctx = canvas.getContext('2d');
 		// 이후 스트림 종료 이벤트를 위해 아래의 변수를 저장하여 활용합니다
 		let track: MediaStreamTrack | null = null;
-		let interval: NodeJS.Timer | null = null;
 
 		async function startCamera() {
 			// 먼저 스트림 객체를 받아옵니다
 			const stream = await getStream(0);
 			if (!stream) return;
 			await reader.decodeFromStream(stream, video, result => {
-				// TODO: 바코드 요청보내도록 수정
-				if (result) console.dir(result.getText());
-			});
-
-			// 비디오 시작 시 비디오 크기로 캔버스의 크기를 조절합니다
-			video.addEventListener('play', () => {
-				const maxSize = Math.min(window.innerWidth, 400);
-				const videoSize = Math.min(video.videoHeight, video.videoWidth);
-
-				// 캔버스 설정
-				canvas.width = Math.min(videoSize, maxSize) - 32;
-				canvas.height = canvas.width / 2;
-				canvas.style.transform = `scale(${video.clientHeight / video.videoHeight})`;
+				if (result) {
+					const newCode = result.getText();
+					if (code === newCode) return;
+					// if (barcodeQuery.isLoading) return;
+					setCode(newCode);
+				}
 			});
 
 			[track] = stream.getVideoTracks();
@@ -55,40 +61,22 @@ function useBarcode() {
 					advanced: [{ focusMode: 'continuous' }],
 				});
 			}
-
-			// 스트림에 따라 캔버스를 업데이트할 수 있도록 인터벌 객체를 선언합니다
-			interval = setInterval(() => {
-				const dx = -video.videoWidth / 2 + canvas.width / 2;
-				const dy = -video.videoHeight / 2 + canvas.height / 2;
-
-				if (!ctx) return;
-				ctx.drawImage(video, dx, dy, video.videoWidth, video.videoHeight);
-			});
 		}
 
 		startCamera();
 
 		return () => {
-			if (interval) clearInterval(interval);
 			if (track) track.stop();
 			video.pause();
 		};
 	}, []);
-
-	/** 촬영 클릭 함수 */
-	const onCapture = () => {
-		const canvas = document.getElementsByTagName('canvas')[0];
-
-		const dataURL = canvas.toDataURL('image/jpg');
-		dispatch(getBarcodeData(dataURL));
-	};
 
 	/** 직접 입력 이동 클릭 함수 */
 	const onHanWriting = () => {
 		dispatch(useEmptyBarcodeData());
 	};
 
-	return [onCapture, onHanWriting];
+	return [onHanWriting];
 }
 
 export default useBarcode;
--- fe/src/store/modules/barcode.ts
+++ fe/src/store/modules/barcode.ts
@@ -1,5 +1,5 @@
 import { ActionReducerMapBuilder, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { postFoodBarcode } from '../../apis/foods';
+import { getFoodBarcode } from '../../apis/foods';
 import { RootState } from '../store';
 
 export interface BarcodeState {
@@ -7,7 +7,6 @@ export interface BarcodeState {
 	barcode: string | null;
 	foodName: string;
 	foodCategoryId: number;
-	foodId: number;
 	pogDaycnt: number;
 }
 
@@ -15,23 +14,16 @@ const initialState: BarcodeState = {
 	status: 'default',
 	barcode: null,
 	foodCategoryId: -1,
-	foodId: -1,
 	foodName: '',
 	pogDaycnt: -1,
 };
 
 // 바코드 사진을 통한 정보 요청 함수
-export const getBarcodeData = createAsyncThunk('barcode/getBarcodeData', async (dataURL: string) => {
+export const getBarcodeData = createAsyncThunk('barcode/getBarcodeData', async (code: string) => {
 	// dataURL 형태의 이미지를 file로 변환합니다
-	const blobBin = atob(dataURL.split(',')[1]);
-	const array: number[] = [];
-	blobBin.split('').forEach(chr => array.push(chr.charCodeAt(0)));
-
-	const blob = new Blob([new Uint8Array(array)], { type: 'image/jpg' });
-	const file = new File([blob], 'image.jpg');
 
 	// 변환된 file을 통해 요청을 보냅니다
-	const { data } = await postFoodBarcode(file);
+	const { data } = await getFoodBarcode(code);
 
 	// 유효하지 않은 바코드인 경우
 	if (data.resultCode === '400') {
@@ -42,9 +34,8 @@ export const getBarcodeData = createAsyncThunk('barcode/getBarcodeData', async (
 	if (data.resultCode === '200') {
 		const barcode: BarcodeState = {
 			status: 'success',
-			barcode: data.data.barcode,
-			foodCategoryId: data.data.foodCategory.id,
-			foodId: data.data.id,
+			barcode: code,
+			foodCategoryId: data.data.foodCategoryId,
 			foodName: data.data.foodName,
 			pogDaycnt: data.data.pogDaycnt,
 		};
@@ -62,7 +53,6 @@ export const barcodeSlice = createSlice({
 			temp.status = 'default';
 			temp.barcode = null;
 			temp.foodCategoryId = -1;
-			temp.foodId = -1;
 			temp.foodName = '';
 			temp.pogDaycnt = -1;
 		},
@@ -80,13 +70,11 @@ export const barcodeSlice = createSlice({
 		});
 		// fullfilled
 		builder.addCase(getBarcodeData.fulfilled, (state, { payload }) => {
-			const { status, barcode, foodCategoryId, foodId, foodName, pogDaycnt } = JSON.parse(payload);
-
+			const { status, barcode, foodCategoryId, foodName, pogDaycnt } = JSON.parse(payload);
 			const temp = state;
 			temp.status = status;
 			temp.barcode = barcode;
 			temp.foodCategoryId = foodCategoryId;
-			temp.foodId = foodId;
 			temp.foodName = foodName;
 			temp.pogDaycnt = pogDaycnt;
 		});
