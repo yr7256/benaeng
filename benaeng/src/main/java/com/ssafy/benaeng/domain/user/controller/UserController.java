e7d7d775ccff69c6e133e458b04ef8565a5fc438
seunghee <kanada11416@gmail.com>
2023-05-11 15:08:37 +0900

    🚚 rename. loginUserDto-> UserDto, UserDto-> UpdateUserDto

--- benaeng/src/main/java/com/ssafy/benaeng/domain/user/controller/UserController.java
+++ benaeng/src/main/java/com/ssafy/benaeng/domain/user/controller/UserController.java
@@ -1,17 +1,12 @@
 package com.ssafy.benaeng.domain.user.controller;
 
-import com.ssafy.benaeng.domain.user.entity.User;
-import com.ssafy.benaeng.domain.user.requestDto.UpdateUserDto;
+import com.ssafy.benaeng.domain.user.responseDto.UpdateUserDto;
 import com.ssafy.benaeng.domain.user.responseDto.UserDto;
-import com.ssafy.benaeng.domain.user.responseDto.loginUserDto;
 import com.ssafy.benaeng.domain.user.service.UserService;
 import com.ssafy.benaeng.global.responseDto.CommonDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -27,17 +22,17 @@ public class UserController {
     @GetMapping("/social/{code}")
     public CommonDto<Object> code(@PathVariable("code") String code, HttpServletResponse response) throws Exception{
         try {
-            loginUserDto user = userService.login(code, response);
+            UserDto user = userService.login(code, response);
             return CommonDto.of("200", "login 성공", user);
         }catch (RuntimeException re){
             return CommonDto.of("400", "login 실패", re.getMessage());
         }
     }
     @PutMapping("/user")
-    public CommonDto<Object> update(@AuthenticationPrincipal String userId, @RequestBody UpdateUserDto updateUserDto){
+    public CommonDto<Object> update(@AuthenticationPrincipal String userId, @RequestBody com.ssafy.benaeng.domain.user.requestDto.UpdateUserDto updateUserDto){
         Long id = Long.parseLong(userId);
         try {
-            UserDto user = userService.updateUser(id, updateUserDto);
+            UpdateUserDto user = userService.updateUser(id, updateUserDto);
             return CommonDto.of("200", "사용자 정보 변경 성공", user);
         }catch(RuntimeException re){
             return CommonDto.of("400", "사용자 정보 변경 실패", re.getMessage());
