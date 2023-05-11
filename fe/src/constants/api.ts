b10017f68e620e06cd999743239005d4acd4773b
한유경 <ygo65312@naver.com>
2023-05-11 15:25:31 +0900

    🐛 fixed bug : delete non exist import

--- fe/src/constants/api.ts
+++ fe/src/constants/api.ts
@@ -2,7 +2,8 @@ export const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
 export const REDIRECT_URI = 'https://k8b205.p.ssafy.io/login';
 
 // 기본 base 경로
-export const BASE_URL = 'https://k8b205.p.ssafy.io';
+// export const BASE_URL = 'https://k8b205.p.ssafy.io';
+export const BASE_URL = 'http://192.168.31.134:8080';
 
 // stale time
 export const STALE_TIME = 1000 * 60 * 5;
