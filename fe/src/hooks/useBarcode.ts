1bf36bf746c69568d7022b2ee5d4d5e4e032ce2d
한유경 <ygo65312@naver.com>
2023-05-11 15:22:58 +0900

    🎨 update camera style & upgrade scan barcode logic

--- fe/src/hooks/useBarcode.ts
+++ fe/src/hooks/useBarcode.ts
@@ -1,26 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
-import { BarcodeFormat, DecodeHintType, BrowserMultiFormatReader } from '@zxing/library';
+import { BrowserMultiFormatReader, IScannerControls } from '@zxing/browser';
+import { BarcodeFormat, DecodeHintType } from '@zxing/library';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { resetBarcodeData, useEmptyBarcodeData, getBarcodeData, selectBarcode } from '../store/modules/barcode';
 import getStream from '../utils/camera';
 import { useAppDispatch, useAppSelector } from './useStore';
 
+declare global {
+	interface MediaTrackConstraintSet {
+		focusMode?: 'continuous' | 'single-shot' | 'manual' | 'none';
+	}
+}
+
 function useBarcode() {
 	const barcode = useAppSelector(selectBarcode);
 	const navigate = useNavigate();
-
 	const dispatch = useAppDispatch();
-
 	const [code, setCode] = useState<string>('');
 	useQuery(['barcode', code], () => dispatch(getBarcodeData(code)), {
 		enabled: Boolean(code),
 	});
-
-	const hints = new Map();
-	const formats = [BarcodeFormat.EAN_13, BarcodeFormat.EAN_8, BarcodeFormat.CODE_128];
-	hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
-	const reader = new BrowserMultiFormatReader(hints);
 
 	useEffect(() => {
 		if (barcode.status === 'success') navigate('/');
@@ -32,40 +32,64 @@ function useBarcode() {
 		dispatch(resetBarcodeData);
 
 		const video = document.getElementsByTagName('video')[0];
-		// 이후 스트림 종료 이벤트를 위해 아래의 변수를 저장하여 활용합니다
+		const canvas = document.getElementsByTagName('canvas')[0];
+		const ctx = canvas.getContext('2d', { willReadFrequently: true });
+		// 스트림 종료를 위해 트랙정보를 저장합니다
 		let track: MediaStreamTrack | null = null;
+		// 이후 스트림 종료 이벤트를 위해 아래의 변수를 저장하여 활용합니다
+		let interval: NodeJS.Timer | null = null;
+		let readerControls: IScannerControls | null = null;
 
 		async function startCamera() {
 			// 먼저 스트림 객체를 받아옵니다
 			const stream = await getStream(0);
 			if (!stream) return;
-			await reader.decodeFromStream(stream, video, result => {
-				if (result) {
-					const newCode = result.getText();
-					if (code === newCode) return;
-					// if (barcodeQuery.isLoading) return;
-					setCode(newCode);
-				}
-			});
+			video.srcObject = stream;
 
+			// 오토 포커싱을 위한 설정 추가합니다
 			[track] = stream.getVideoTracks();
-			// 오토 포커싱을 위한 설정 추가
 			const constraints = track.getConstraints();
 
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
 			if (constraints.focusMode) {
 				track.applyConstraints({
-					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-					// @ts-ignore
 					advanced: [{ focusMode: 'continuous' }],
 				});
 			}
+
+			// 스트림에 따라 캔버스를 업데이트할 수 있도록 인터벌 객체를 선언합니다
+			interval = setInterval(() => {
+				const size = video.clientHeight / video.videoHeight;
+				const width = video.videoWidth * size;
+				const height = video.videoHeight * size;
+				const dx = -width / 2 + canvas.width / 2;
+				const dy = -height / 2 + canvas.height / 2;
+
+				if (!ctx) return;
+				ctx.drawImage(video, dx, dy, width, height);
+			});
+
+			// zxing를 이용한 바코드인식 이벤트 등록
+			const hints = new Map();
+			const formats = [BarcodeFormat.EAN_13, BarcodeFormat.EAN_8, BarcodeFormat.CODE_128];
+			hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
+			const reader = new BrowserMultiFormatReader(hints);
+
+			readerControls = await reader.decodeFromVideoElement(video, result => {
+				if (result) {
+					const newCode = result.getText();
+					console.log(newCode);
+					if (code === newCode) return;
+					if (barcode.status === 'loading') return;
+					setCode(newCode);
+				}
+			});
 		}
 
 		startCamera();
 
 		return () => {
+			if (readerControls) readerControls.stop();
+			if (interval) clearInterval(interval);
 			if (track) track.stop();
 			video.pause();
 		};
