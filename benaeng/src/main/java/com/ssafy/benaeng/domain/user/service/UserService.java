a0cb7d70a02c352e8ef648d213668859687d93ab
seunghee <kanada11416@gmail.com>
2023-05-12 10:03:22 +0900

    :coffin: remove dead code

--- benaeng/src/main/java/com/ssafy/benaeng/domain/user/service/UserService.java
+++ benaeng/src/main/java/com/ssafy/benaeng/domain/user/service/UserService.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
