a102e54cdd8de55c214ab84210c2d88b4684e85c
yr7256 <yr7256@gmail.com>
2023-05-15 13:37:13 +0900

    🎨 edit calendar

--- fe/src/components/analysis/RefrigeratorCalendar.tsx
+++ fe/src/components/analysis/RefrigeratorCalendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { throttle } from 'lodash';
 import Calendar from './Calendar/Main';
@@ -42,6 +42,8 @@ function RefrigeratorCalendar() {
 	// const totalRecords: string[] = Array.from(recordSet);
 	// const totalCycles: string[] = Array.from(cycleSet);
 
+	const today = new Date();
+	const [selectedDatePurchases, setSelectedDatePurchases] = useState<Date>(today);
 	const totalRecords: { [key: string]: number[] } = {};
 	const totalCycles: { [key: string]: number[] } = {};
 
@@ -68,9 +70,27 @@ function RefrigeratorCalendar() {
 		return categoryData ? categoryData.subCategory : '';
 	};
 
+	const dateToyyyymmdd = (date: Date): string => {
+		const year = date.getFullYear();
+		const month = String(date.getMonth() + 1).padStart(2, '0');
+		const day = String(date.getDate()).padStart(2, '0');
+
+		return `${year}-${month}-${day}`;
+	};
+
+	const dateTommdd = (date: Date): string => {
+		const month = String(date.getMonth() + 1);
+		const day = String(date.getDate());
+		return `${month}/${day}`;
+	};
+
+	const filteredPurchases = CalendarDataQuery.purchase.filter(data =>
+		data.purchaseRecords.includes(dateToyyyymmdd(selectedDatePurchases)),
+	);
+
 	return (
 		<div className="mt-6">
-			<Calendar purchase={totalRecords} cycle={totalCycles} />
+			<Calendar purchase={totalRecords} cycle={totalCycles} setSelectedDatePurchases={setSelectedDatePurchases} />
 			<div className="flex text-sm font-bold text-green mb-3 min-w-75.5 max-w-88">슬슬 구매해야 할 항목</div>
 			{CalendarDataQuery.alarm.map(data => (
 				<div className="mb-3">
@@ -84,16 +104,23 @@ function RefrigeratorCalendar() {
 				</div>
 			))}
 			{/* // 음식명, 소분류, 알림 타입, d-day, 음식 id */}
-			<div className="flex text-sm font-bold text-yellow mt-3 mb-3 min-w-75.5 max-w-88">오늘 구매한 항목</div>
+			<div className="flex text-sm font-bold text-yellow mt-3 mb-3 min-w-75.5 max-w-88">
+				{dateTommdd(selectedDatePurchases) === dateTommdd(today) ? '오늘' : dateTommdd(selectedDatePurchases)} 구매한
+				항목
+			</div>
 			<div className="flex component px-5 py-6 flex-wrap stroke border">
-				{CalendarDataQuery.purchase.map(data => (
-					<div className="flex w-1/4">
-						<div className="flex mx-auto my-2 flex-col text-xs font-bold">
-							<FoodIcon food={getSubCategory(data.categoryId)} size="lg" />
-							<p className="mt-2 text-left">{data.foodName}</p>
+				{filteredPurchases.length > 0 ? (
+					filteredPurchases.map(data => (
+						<div className="flex w-1/4">
+							<div className="flex mx-auto my-2 flex-col text-xs font-bold">
+								<FoodIcon food={getSubCategory(data.categoryId)} size="lg" />
+								<p className="mt-2 text-left">{data.foodName}</p>
+							</div>
 						</div>
-					</div>
-				))}
+					))
+				) : (
+					<p>구매한 상품이 없어요.</p>
+				)}
 			</div>
 		</div>
 	);
