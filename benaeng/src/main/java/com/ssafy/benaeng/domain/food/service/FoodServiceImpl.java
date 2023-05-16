56bd9473cf5fc27a8331d5f038415c3a6b7aa52b
유현근 <jatyu62@gmail.com>
2023-05-16 11:02:49 +0900

    🎨 update foodsave service

--- benaeng/src/main/java/com/ssafy/benaeng/domain/food/service/FoodServiceImpl.java
+++ benaeng/src/main/java/com/ssafy/benaeng/domain/food/service/FoodServiceImpl.java
@@ -41,45 +41,45 @@ public class FoodServiceImpl implements FoodService{
         myFood.setTotalCount(registDto.getTotalCount());
         myFood.setCount(registDto.getTotalCount());
         myFood.setUser(userRepository.findById(registDto.getUserId()).orElseThrow());
-        if(registDto.getIsRecommend()){
-            String barcode = registDto.getBarcode();
-            List<FoodData> foodDataList  = foodDataRepository.findAllByBarcode(barcode);
-            FoodData foodData = foodDataList.get(0);
-            String day = foodData.getPogDaycnt();
-            int dayCnt = Integer.parseInt(day);
-            LocalDate currentDate = LocalDate.now();
-            Date startDate = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-            LocalDate endDateLocal = currentDate.plusDays(dayCnt);
-            Date endDate = Date.from(endDateLocal.atStartOfDay(ZoneId.systemDefault()).toInstant());
-
-            myFood.setStartDate(startDate);
-            myFood.setEndDate(endDate);
-            log.info("소비기한 계산로직이 들어갈예정입니다.");
-            return myFood;
-        }
-        else{
-            if(!registDto.getIsConsume()) {
-                String barcode = registDto.getBarcode();
-                List<FoodData> foodDataList  = foodDataRepository.findAllByBarcode(barcode);
-                FoodData foodData = foodDataList.get(0);
-                String day = foodData.getPogDaycnt();
-                int dayCnt = Integer.parseInt(day);
-                LocalDate currentDate = LocalDate.now();
-                Date startDate = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-                LocalDate endDateLocal = currentDate.plusDays(dayCnt);
-                Date endDate = Date.from(endDateLocal.atStartOfDay(ZoneId.systemDefault()).toInstant());
-
-                myFood.setStartDate(startDate);
-                myFood.setEndDate(endDate);
-                log.info("유통기한 기반 계산로직이 들어갈예정입니다.");
-                return null;
-            }
-            else{
-                log.info("소비기한을 입력했으므로 바로 저장합니다.");
-                myFood.setStartDate(registDto.getStartDate());
-                myFood.setEndDate(registDto.getEndDate());
-            }
-        }
+//        if(registDto.getIsRecommend()){
+//            String barcode = registDto.getBarcode();
+//            List<FoodData> foodDataList  = foodDataRepository.findAllByBarcode(barcode);
+//            FoodData foodData = foodDataList.get(0);
+//            String day = foodData.getPogDaycnt();
+//            int dayCnt = Integer.parseInt(day);
+//            LocalDate currentDate = LocalDate.now();
+//            Date startDate = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+//            LocalDate endDateLocal = currentDate.plusDays(dayCnt);
+//            Date endDate = Date.from(endDateLocal.atStartOfDay(ZoneId.systemDefault()).toInstant());
+//
+//            myFood.setStartDate(startDate);
+//            myFood.setEndDate(endDate);
+//            log.info("소비기한 계산로직이 들어갈예정입니다.");
+//            return myFood;
+//        }
+//        else{
+//            if(!registDto.getIsConsume()) {
+//                String barcode = registDto.getBarcode();
+//                List<FoodData> foodDataList  = foodDataRepository.findAllByBarcode(barcode);
+//                FoodData foodData = foodDataList.get(0);
+//                String day = foodData.getPogDaycnt();
+//                int dayCnt = Integer.parseInt(day);
+//                LocalDate currentDate = LocalDate.now();
+//                Date startDate = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+//                LocalDate endDateLocal = currentDate.plusDays(dayCnt);
+//                Date endDate = Date.from(endDateLocal.atStartOfDay(ZoneId.systemDefault()).toInstant());
+//
+//                myFood.setStartDate(startDate);
+//                myFood.setEndDate(endDate);
+//                log.info("유통기한 기반 계산로직이 들어갈예정입니다.");
+//                return null;
+//            }
+//            else{
+//        log.info("소비기한을 입력했으므로 바로 저장합니다.");
+        myFood.setStartDate(registDto.getStartDate());
+        myFood.setEndDate(registDto.getEndDate());
+//            }
+//        }
         log.info("음식 {} (이)가 {}의 냉장고에 저장됩니다.",myFood.getFoodName() , myFood.getUser().getName());
         myfoodRepository.save(myFood);
         return myFood;
