138672861169cacdbf14085602357cc0f7f67a52
seunghee <kanada11416@gmail.com>
2023-05-16 13:11:08 +0900

    :bug: add @Transactional

--- benaeng/src/main/java/com/ssafy/benaeng/domain/food/repository/alarm/AlarmRepository.java
+++ benaeng/src/main/java/com/ssafy/benaeng/domain/food/repository/alarm/AlarmRepository.java
@@ -2,11 +2,13 @@ package com.ssafy.benaeng.domain.food.repository.alarm;
 
 import com.ssafy.benaeng.domain.food.entity.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long>, AlarmRepositoryCustom{
+    @Transactional
     void deleteByUserId(Long id);
 }
