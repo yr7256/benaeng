c032e28be9fcd2f40f989e3f2a306d08c7560df0
서예지 <syg9272@naver.com>
2023-05-17 10:43:36 +0900

    👽️ add put alarm

--- fe/src/apis/alarm.ts
+++ fe/src/apis/alarm.ts
@@ -3,9 +3,12 @@ import { AlarmData, Response } from '../types';
 
 export const ALARM_API = '/alarm';
 
-/** [GET] 식품별 분석 조회 */
-function getAlarm() {
+/** [GET] 알람 목록 조회 */
+export function getAlarm() {
 	return getInstance().get<Response<AlarmData[]>>(`${ALARM_API}`);
 }
 
-export default getAlarm;
+/** [PUT] 알람 읽음 처리 */
+export function putAlarm() {
+	return getInstance().put<Response<null>>(`${ALARM_API}`);
+}
