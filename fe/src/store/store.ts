befc7ef4309788147f23287e9f75ec11b86bba9c
서예지 <syg9272@naver.com>
2023-05-17 13:19:49 +0900

    🎨 update user store

--- fe/src/store/store.ts
+++ fe/src/store/store.ts
@@ -1,34 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
-// import storage from 'redux-persist/lib/storage';
-// import { persistReducer } from 'redux-persist'; // redux-persist (새로고침 해도 유지)
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist'; // redux-persist (새로고침 해도 유지)
 import user from './modules/user';
 import barcode from './modules/barcode';
 
-// const reducers = combineReducers({
-// 	user,
-// 	barcode,
-// });
+const reducers = combineReducers({
+	user,
+	barcode,
+});
 
-// const persistConfig = {
-// 	key: 'root',
-// 	storage,
-// };
+const persistConfig = {
+	key: 'root',
+	storage,
+};
 
-// const persistedReducer = persistReducer(persistConfig, reducers);
-
-// const store = configureStore({
-// 	reducer: persistedReducer,
-// 	middleware: getDefaultMiddleware =>
-// 		getDefaultMiddleware({
-// 			serializableCheck: false,
-// 		}),
-// });
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-	reducer: {
-		user,
-		barcode,
-	},
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
