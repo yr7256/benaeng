a7eb38ca8768126885b8b535eed5a890978ffcfc
한유경 <ygo65312@naver.com>
2023-05-18 11:17:29 +0900

    🎨 update toggle component

--- fe/src/pages/Setting.tsx
+++ fe/src/pages/Setting.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 // import { Cookies } from 'react-cookie';
 import Topbar from '../components/common/topbar/Topbar';
 import { useAppDispatch, useAppSelector } from '../hooks/useStore';
-import { logout, selectUser, userSlice } from '../store/modules/user';
+import { logout, selectUser, setIsAlarm, setIsCycle, setIsDark, setIsPurchase, userSlice } from '../store/modules/user';
 import Toggle from '../components/common/toggle/Toggle';
 import { usePutUser } from '../apis/user';
 import sendToken from '../apis/token';
@@ -107,27 +107,47 @@ function Setting() {
 			<Topbar />
 			<div className="flex items-center justify-between px-6 py-3 mb-4 border-2 rounded-2xl stroke bg-light/component dark:bg-dark/component text-light/text dark:text-dark/text">
 				<div>다크모드</div>
-				<Toggle isCheck={false} onState={userInfo.isDark} type="isDark" />
+				<Toggle
+					label="isDark"
+					disabled={false}
+					value={userInfo.isDark}
+					onToggle={() => dispatch(setIsDark(!userInfo.isDark))}
+					className=""
+				/>
 			</div>
 			<div className="mb-4 border-2 rounded-2xl stroke bg-light/component dark:bg-dark/component text-light/text dark:text-dark/text">
 				<div className="flex items-center justify-between px-6 py-3 border-b-2 stroke">
 					<div>알림 받기</div>
-					<Toggle isCheck={false} onState={userInfo.isAlarm} type="isAlarm" />
+					<Toggle
+						label="isAlarm"
+						disabled={false}
+						value={userInfo.isAlarm}
+						onToggle={() => dispatch(setIsAlarm(!userInfo.isAlarm))}
+						className=""
+					/>
 				</div>
 				<div className="flex items-center justify-between px-6 py-3">
 					<div className={`${userInfo.isAlarm ? 'text-text' : 'text-light/stroke dark:text-dark/stroke'}`}>
 						소비기한 임박 식품 알림
 					</div>
-					<Toggle isCheck={!userInfo.isAlarm} onState={userInfo.isAlarm ? userInfo.isCycle : false} type="isCycle" />
+					<Toggle
+						label="isCycle"
+						disabled={!userInfo.isAlarm}
+						value={userInfo.isAlarm ? userInfo.isCycle : false}
+						onToggle={() => dispatch(setIsCycle(!userInfo.isCycle))}
+						className=""
+					/>
 				</div>
 				<div className="flex items-center justify-between px-6 py-3">
 					<div className={`${userInfo.isAlarm ? 'text-text' : 'text-light/stroke dark:text-dark/stroke'}`}>
 						식품 구매주기 알림
 					</div>
 					<Toggle
-						isCheck={!userInfo.isAlarm}
-						onState={userInfo.isAlarm ? userInfo.isPurchase : false}
-						type="isPurchase"
+						label="isPurchase"
+						disabled={!userInfo.isAlarm}
+						value={userInfo.isAlarm ? userInfo.isPurchase : false}
+						onToggle={() => dispatch(setIsPurchase(!userInfo.isPurchase))}
+						className=""
 					/>
 				</div>
 			</div>
