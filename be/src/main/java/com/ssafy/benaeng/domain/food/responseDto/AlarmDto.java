acc306b03510cb1f2d03d9066f8095e01029f128
seunghee <kanada11416@gmail.com>
2023-05-19 11:12:23 +0900

    :bug: fix timezone error

--- be/src/main/java/com/ssafy/benaeng/domain/food/responseDto/AlarmDto.java
+++ be/src/main/java/com/ssafy/benaeng/domain/food/responseDto/AlarmDto.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class AlarmDto {
     private Integer type;
     private Integer status;
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private Date createDate;
     private Integer dDay;
     private String foodName;
